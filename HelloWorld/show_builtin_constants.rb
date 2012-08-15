3fa242b75f83d895298f948bc18c87dbaa612873
takewo <st0098@gmail.com>
2012-08-15 11:15:15 +0900

    using builtin constants and pseudo variables

---
+++ HelloWorld/show_builtin_constants.rb
@@ -0,0 +1,31 @@
+# 引数または標準入力により構成される仮想ファイル
+# なんのこっちゃ？
+puts ARGF
+
+#コマンドライン引数
+puts ARGV
+
+#環境変数
+puts ENV
+
+#Ruby処理系のバージョン
+puts RUBY_VERSION
+
+#Ruby処理系のリリース日
+puts RUBY_RELEASE_DATE
+
+#実行環境
+puts RUBY_PLATFORM
+
+#標準入力
+puts STDIN
+
+#標準出力
+puts STDOUT
+
+#標準エラー出力
+puts STDERR 
+
+# __END__ 以降を扱うオブジェクト
+# なんのこっちゃ？
+puts DATA
---
+++ HelloWorld/show_pseudo_variables.rb
@@ -0,0 +1,15 @@
+# 値がないことを示す。nullと同じ
+puts nil
+
+# 真偽値
+puts true
+puts false
+
+# メソッド実行主体を表す (Javaでいうthis)
+puts self
+
+# 実行中プログラムのそーす　ファイル名
+puts __FILE__
+
+# 実行中プログラムの行番号
+puts __LINE__
