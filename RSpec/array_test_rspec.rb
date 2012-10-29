a5676d37d0a82b427b9dedfc7d164b8ae0db003c
takewo <st0098@gmail.com>
2012-10-29 14:52:33 +0900

    added RSpec structure comment

--- RSpec/array_test_rspec.rb
+++ RSpec/array_test_rspec.rb
@@ -1,20 +1,29 @@
+# Behaviour - 1
 describe Array, "when empty" do
   before do
     @empty_array = []
   end
 
+  # Example - 1
   it "sholud be empty" do
+    # Expectation - 1-1
     @empty_array.should be_empty
   end
 
+  # Example - 2
   it "should size 0" do
+    # Expectation - 2-1
     @empty_array.size.should == 0
   end
 
+  # Example - 3
   after do
+    # Expectation - 3-1
     @empty_array = nil
   end
 end
+
+# Behaviour - 2
 describe Array, "when initialized with object" do
   before do
     #@array = Array.new(3, Hash.new) #これは勘違い
@@ -25,7 +34,9 @@ describe Array, "when initialized with object" do
     @array[0][:cat] = "Nuko"
   end
 
+  # Example - 1
   it "should not affect others" do
+    # Expectation - 1
     @array.should == [{:cat => "Nuko"}, {}, {}]
   end
 end
