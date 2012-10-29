0e2c00a5da8faca0bd39f9aa6e32931e53c63f1b
takewo <st0098@gmail.com>
2012-10-29 17:44:59 +0900

    created sample rails project for RSpec study

---
+++ RSpec/myblog-rspec-sample/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyblogRspecSample::Application.config.secret_token = '40c758bce1e175dbda88521badb8ad883136d93aa8c3b9f87559bd5184125f6103df915ab3d46de1b933eba8e38c46bbfdc9b06e015c5bb754c869431fc78078'
